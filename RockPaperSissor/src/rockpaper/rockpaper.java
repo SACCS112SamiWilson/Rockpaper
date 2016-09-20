b203bb34e3892aa842dd02983c8e984121a1df21
SACCS112SamiWilson <samiwilson7771@hotmail.com>
2016-09-20 08:20:35 -0700

    Add files via upload

---
+++ RockPaperSissor/src/rockpaper/rockpaper.java
@@ -0,0 +1,12 @@
+package rockpaper;
+
+class rockpaper
+{
+   public static void main(String args[])
+   {
+	   var userChoice =  prompt("Do you choose rock, paper or scissors?")
+			   var userChoice1 =  prompt("Do you choose rock, paper or scissors?")
+			   computerChoice = Math.random()
+			   console.log(computerChoice)	   
+   }
+}
